9d7abc53b98757dd7437cf0ea199577586fda310
benjamin <bupark82@gmail.com>
2021-07-09 12:55:04 +0900

    7/9 git class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
+
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+        # return render(request, 'accountapp/hello_world.html', context={'text': temp})
+        return render(request, 'accountapp/hello_world.html', context={'text': new_model})
+
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
