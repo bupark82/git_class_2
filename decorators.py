ee9acb84137d129702d7782c18a02c50a4ac4d38
benjamin <bupark82@gmail.com>
2021-07-21 18:14:09 +0900

    7/21 git class commit 4

--- decorators.py
+++ decorators.py
@@ -12,3 +12,21 @@ def hello_world(input_text):
 
 
 hello_world('Hello World!')
+
+def check_integer(func):
+    def decoratored(input_h, input_v):
+        if input_h > 0 and input_v > 0 :
+            func(input_h, input_v)
+        else :
+            raise valueError('Input must be positive value')
+    return decoratored
+
+@check_integer
+def rect_area(input_h, input_v):
+    return input_h * input_v
+
+@check_integer
+def tri_area(input_h, input_v):
+    return 1/2 * input_h * input_v
+rect_area(3,4)
+tri_area(3,4)
