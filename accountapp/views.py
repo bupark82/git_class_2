280e5f51c49367b34c9f8607fcc346c292827a6f
benjamin <bupark82@gmail.com>
2021-08-27 11:18:46 +0900

    08/27 git class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -15,34 +15,10 @@ from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 from articleapp.models import Article
 
-
-@login_required
-def hello_world(request):
-    # if request.user.is_authenticated:
-    if request.method == "POST":
-        temp = request.POST.get('input_text')
-
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
-
-        # data_list = NewModel.objects.all()
-        # return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
-        # return render(request, 'accountapp/hello_world.html', context={'text': temp})
-        # return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-        # return HttpResponseRedirect('accountapp:hello_world')
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-    # else:
-    #     return HttpResponseRedirect(reverse('accountapp:login'))
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
     # def get_success_url(self):
@@ -94,7 +70,7 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
     # def get(self, request, *args, **kwargs):
